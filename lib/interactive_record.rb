34780b81a35cf13fdf73e7e5e6b545ff9d651fad
Anthony Leon <acidknights89@gmail.com>
2020-11-15 00:26:22 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,68 +4,68 @@ require 'pry'
 
 class InteractiveRecord
 
-  def self.table_name
-     self.to_s.downcase.pluralize
+   def self.table_name
+     "#{self.to_s.downcase}s"
    end
 
    def self.column_names
-     DB[:conn].results_as_hash = true
-
-     sql = "pragma table_info ('#{table_name}')"
-
+     sql = "PRAGMA table_info(#{table_name});"
      table_info = DB[:conn].execute(sql)
-     column_names = []
-     table_info.each do |row|
-       column_names << row["name"]
-     end
-     column_names.compact
-   end
 
-   self.column_names.each do |col_name|
-     attr_accessor col_name.to_sym
-   end
-
-   def initialize(options = {})
-     options.each do |property, value|
-       self.send("#{property}=", value)
-     end
-   end
-
-   def self.find_by(hash)
-     key, value = hash.first
-     # if value.is_a? String
-     #   value = "'#{value}'"
-     # end
-     # binding.pry
-     sql = "SELECT * FROM #{self.table_name} WHERE #{key.to_s} = ?"
-     DB[:conn].execute(sql, value)
+     table_info.map { |column_info| column_info['name'] }.compact
    end
 
    def self.find_by_name(name)
-     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-     DB[:conn].execute(sql)
+     sql = "SELECT * FROM #{table_name} WHERE name = ?"
+     DB[:conn].execute(sql, name)
+   end
+
+   def self.find_by(attributes_hash)
+     criteria = attributes_hash.keys.map { |column| "#{column} = ?"  }.join(' AND ')
+     sql = "SELECT * FROM #{table_name} WHERE #{criteria}"
+
+     DB[:conn].execute(sql, *attributes_hash.values)
+   end
+
+   def initialize(options = {})
+     options.each do |key, value|
+       send("#{key}=", value)
+     end
    end
 
    def table_name_for_insert
      self.class.table_name
    end
 
-   def values_for_insert
-     values = []
-     self.class.column_names.each do |col_name|
-       values << "'#{send(col_name)}'" unless send(col_name).nil?
-     end
-     values.join(", ")
+   def col_names_for_insert
+     self.class.column_names - ['id']
    end
 
-   def col_names_for_insert
-     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+   def values_for_insert
+     col_names_for_insert.map do |col|
+       col_value = send(col)
+     end
    end
+
+   # saves the student to the db
 
    def save
-     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-     DB[:conn].execute(sql)
-     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+     insert
    end
 
+   def insert
+     sql = <<~SQL
+           INSERT INTO #{table_name_for_insert}
+           (#{col_names_for_insert.join(', ')})
+           VALUES (#{value_placeholders})
+           SQL
+
+     DB[:conn].execute(sql, values_for_insert)
+     id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")
+     self.id = id.first['last_insert_rowid()']
+   end
+
+   def value_placeholders
+     (1..col_names_for_insert.size).map {'?'}.join(',')
+   end
  end
